88440ae531c31d2da41363d51c09e45734712a41
kyleQin <kyleqinmail@gmail.com>
2022-01-26 09:59:48 +0800

    2022.1.26 commit

---
+++ db/mysql_db.py
@@ -0,0 +1,13 @@
+import mysql.connector.pooling
+
+__mysql_config = {
+    "host": "192.168.1.88",
+    "port": 3308,
+    "user": "root",
+    "password": "123456",
+    "database": "vega"
+}
+
+
+
+
